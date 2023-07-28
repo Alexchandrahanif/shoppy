33c94590c452020dbd2158fbc1f528843556acd0
Alex Chandra Hanif <alexchandra1998@gmail.com>
2023-07-28 11:35:03 +0700

    update terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,122 @@
-import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  Route,
-  Routes,
-  RouterProvider,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import "./App.css";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
   Area,
   Bar,
+  Pie,
+  Financial,
   ColorPicker,
   ColorMapping,
-  Financial,
-  Line,
-  Pie,
-  Pyramid,
-  Stacked,
-  Calendar,
-  Customers,
-  Ecommerce,
   Editor,
-  Employees,
-  Kanban,
-  Orders,
 } from "./pages";
+import "./App.css";
 
 import { useStateContext } from "./contexts/ContextProvider";
 
-function App() {
-  const { activeMenu } = useStateContext();
-  
+const App = () => {
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
   return (
-    <div>
-      <div>
-        <BrowserRouter>
-          <div className="flex relative dark:bg-main-dark-bg">
-            {/* Tombol Settings */}
-            <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-              <TooltipComponent content="Settings" position="Top">
-                <button
-                  className="text-xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                  style={{ background: "blue", borderRadius: "50%" }}
-                >
-                  <FiSettings />
-                </button>
-              </TooltipComponent>
-            </div>
-
-            {/* Sidebar */}
-            {activeMenu ? (
-              <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                <Sidebar />
-              </div>
-            ) : (
-              <div className="w-0 dark:bg-secondary-dark-bg bg-white">
-                <Sidebar />
-              </div>
-            )}
-
-            {/* Home */}
-            <div
-              className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-                activeMenu ? "md:ml-72" : "flex-1"
-              }`}
-            ></div>
-
-            {/* Navbar */}
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              {/* <Navbar /> */}
-              navbar
-            </div>
-
-            {/* Routes tapi ini masih cara lama setau saya */}
-            <Routes>
-              {/* Halaman Dashboard */}
-              <Route path="/" element={<Ecommerce />} />
-              <Route path="/ecommerse" element={<Ecommerce />} />
-
-              {/* Halaman/Pages */}
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/employees" element={<Employees />} />
-              <Route path="/customers" element={<Customers />} />
-
-              {/* Aplikasi */}
-              <Route path="/kanban" element={<Kanban />} />
-              <Route path="/editor" element={<Editor />} />
-              <Route path="/calender" element={<Calendar />} />
-              <Route path="/color-picker" element={<ColorPicker />} />
-
-              {/* Charts */}
-              <Route path="/line" element={<Line />} />
-              <Route path="/area" element={<Area />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/financial" element={<Financial />} />
-              <Route path="/color-mapping" element={<ColorMapping />} />
-              <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} />
-            </Routes>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
           </div>
-        </BrowserRouter>
-      </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {themeSettings && <ThemeSettings />}
+
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
@@ -8,62 +8,63 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } =
+    useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white  text-md m-2";
-
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 ";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
-          {/* Logo Dan Close Menu */}
           <div className="flex justify-between items-center">
-            {/* Logo */}
             <Link
-              to={"/"}
-              onClick={() => setActiveMenu(false)}
+              to="/"
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware />
-              <span>Shoppy</span>
+              <SiShopware /> <span>Shoppy</span>
             </Link>
-
-            {/* Close Menu */}
-            <TooltipComponent content={"Menu"} position="ButtomCenter">
+            <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() =>
-                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
-                }
+                onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
           </div>
-
-          {/* Menu */}
-          <div className="mt-10">
+          <div className="mt-10 ">
             {links.map((item) => (
               <div key={item.title}>
-                {/* Menu Utama */}
-                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
-                {/* Sub Menu */}
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
                 {item.links.map((link) => (
                   <NavLink
-                    key={link.name}
                     to={`/${link.name}`}
-                    onClick={() => {}}
+                    key={link.name}
+                    onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : "",
+                    })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
                   >
                     {link.icon}
-                    <span className="capitalize">{link.name}</span>
+                    <span className="capitalize ">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
